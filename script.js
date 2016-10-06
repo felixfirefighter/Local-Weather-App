e43298b4ae7049fefd4267dde99373cd6046a17d
YoongTi <leeyoongti@gmail.com>
2016-10-06 18:55:47 -0400

    Removed previous icons and used Skycons instead Finished project

--- script.js
+++ script.js
@@ -7,78 +7,105 @@
 
 //sunrise 7AM - sunset 7PM
 
+//orange : #FF9800;
+
+
+var lat, lon = 0;
+var arr = [];
+var icon = new Skycons({"color":"white"});
+var temp = 0;
+var isCelsius = true;
+
+
+
 $(document).ready(function () {
+    getLocation();
 
-    var lat, lon = 40;
+    $("#degree").on("click",function () {
+        degreeConvertion();
+    });
 
+});
+
+function getLocation(){
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
             lat = Math.floor(position.coords.latitude);
             lon = Math.floor(position.coords.longitude);
-
-            getWeather(lat, lon);
-
+            getWeather();
         });
-    } else {
-        $(".off").text("Browser Geolocation Not Enabled :(");
-
-        lat = lon = 0;
-
-        getWeather(lat, lon);
     }
+}
 
-
-});
-
-function getWeather(lat, lon){
+function getWeather(){
     var url = "";
-
     var weatherCondition = "";
-
     var date = new Date();
     var hour = date.getHours();
 
-    var text = $("#data");
-    var pic = $("#pic");
-    var test = $("#test");
+    var locationContent = $("#location");
+    var degreeContent = $("#degree");
+
+    var body = $("body");
 
     //url = "http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=212c4c05b3b83f3d3cfc2caad60af394";
-
     //url = "http://api.openweathermap.org/data/2.5/weather?lat=0&lon=0&APPID=212c4c05b3b83f3d3cfc2caad60af394";
+    //url = "http://api.openweathermap.org/data/2.5/weather?lat=3&lon=101&APPID=212c4c05b3b83f3d3cfc2caad60af394";
 
     url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=212c4c05b3b83f3d3cfc2caad60af394&units=metric";
 
     $.getJSON(url,function(data){
 
-        var id = 0;
-
-        var result = JSON.stringify(data);
-
-        var jsonData = JSON.parse(result);
-
-        for(key in result){
-            if(result.hasOwnProperty("coord")){
-                text.append(result["coord"] + "\n");
-            }
-        }
+        var id = data.weather[0].id;
+        var weather = data.weather[0].main;
+        var cityName = data.name;
+        var country = data.sys.country;
+        temp = Math.floor(data.main.temp);
 
         //get current weather condition depending on id
         if((id>=200 && id<=232) || (id>=960 && id<=962)){
-            weatherCondition = "stormy";
+            weatherCondition = Skycons.SLEET;
         }else if((id>=300 && id<=321) || (id>=500 && id<=531)){
-            weatherCondition = "rainy";
+            weatherCondition = Skycons.RAIN;
         }else if(id>=600 && id<=622){
-            weatherCondition = "snowy";
+            weatherCondition = Skycons.SNOW;
         }else if(id == 800){
             if(hour>=7 && hour<=19) {
-                weatherCondition = "sunny";
+                weatherCondition = Skycons.CLEAR_DAY;
             }else{
-                weatherCondition = "starry";
+                weatherCondition = Skycons.CLEAR_NIGHT;
             }
         }else if(id>=801 && id<=804){
-            weatherCondition = "cloudy";
-        }else{
-            weatherCondition = "rainbow";
+            weatherCondition = Skycons.CLOUDY;
+        }else if(id>=952 && id<=962){
+            weatherCondition = Skycons.WIND;
+        } else{
+            weatherCondition = Skycons.FOG;
         }
+
+        icon.set("weather-icon",weatherCondition);
+        icon.play();
+
+        arr = [id,weather,cityName,country];
+
+        locationContent.text(cityName + " , " + country);
+        degreeContent.text(temp + String.fromCharCode(176) + "C");
+
+        body.fadeIn(500);
     });
+}
+
+//T(°F) = T(°C) × 9/5 + 32
+function degreeConvertion(){
+    var degreeContent = $("#degree");
+
+    if(isCelsius){
+        temp = (temp*9/5) + 32;
+        isCelsius = false;
+        degreeContent.text(temp + String.fromCharCode(176) + "F");
+    }else{
+        temp = (temp-32)*5/9;
+        isCelsius = true;
+        degreeContent.text(temp + String.fromCharCode(176) + "C");
+    }
 }
